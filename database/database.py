19bfb8f5b0e5e0745f2358c8c078f2fa3cc591d5
Shaine Berube <shaineberube_computerprogrammer@hotmail.com>
2018-03-04 10:27:16 -0700

    Adding in intial py files for building the system out.

---
+++ database/database.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+import argparse,locale
+import os,sys,inspect,re
+import datetime,socket,json,traceback
+
+# This file is going to be a class with functions that primarily deal with interacting with the database
