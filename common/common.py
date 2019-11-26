017fc152f20d54a3951ecd2551889ff061311066
Shaine Berube <shaine@vorakortech.io>
2019-11-25 21:18:33 -0700

    Destroying old code.

--- common/common.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-import argparse,locale
-import os,sys,inspect,re
-import datetime,socket,json,traceback
-
-# This file is going to be a class with common functions that manipulate data but don't interact directly with the local machine
--- database/database.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-import argparse,locale
-import os,sys,inspect,re
-import datetime,socket,json,traceback
-
-# This file is going to be a class with functions that primarily deal with interacting with the database
